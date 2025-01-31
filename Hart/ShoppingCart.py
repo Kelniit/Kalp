ed4a509968a71ba882c3616f08b6d565e894d8d6
Yoichi Nagumo <kelpin.workspace@gmail.com>
2025-01-31 23:45:27 +0700

    Hi !

---
+++ Hart/ShoppingCart.py
@@ -0,0 +1,44 @@
+class Product:
+  """
+  Shopping Cart Hacker Rank Solution
+
+  Represent Product
+
+  >>> laptop = Product("Laptop", 1200)
+  >>> laptop.price
+  1200
+  """
+  def __init__(self, name, price):
+    self.name = name
+    self.price = price
+
+class YellowCart:
+  """
+  Yellow Cart Shopping
+
+  >>> cart = YellowCart()
+  >>> cart.Total()
+  0
+  >>> laptop = Product("Laptop", 1200)
+  >>> cart.Append(laptop)
+  >>> len(cart)
+  2
+  >>> cart.Total()
+  1200
+  """
+  def __init__(self):
+    self.items = []
+
+  def Total(self):
+    result = sum(item.price for item in self.items)
+    return result
+
+  def Append(self, product : Product):
+    self.items.append(product)
+
+  def __len__(self):
+    return len(self.items)
+
+if __name__ == "__main__":
+  import doctest
+  doctest.testmod()
